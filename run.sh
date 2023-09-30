0d35777861917f7b086c9f1d988670f1d276044a
Pushpakant Behera <pushpakant.b.behera@oracle.com>
2023-09-30 18:51:46 +0530

    added run for macos

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-cmake . && make && ./sha256
+# not able to build for clang with make
+# cmake . && make && ./sha256
 
-# g++ --std=c++2a -I ./include -I ./lib src/main.cc ./src/sha256.cc -o sha256
+# this one works for my machine, installed g++ using brew 🙃
+/opt/homebrew/bin/g++-13 --std=c++23 -I ./include -I ./lib src/main.cc ./src/sha256.cc -o sha256
